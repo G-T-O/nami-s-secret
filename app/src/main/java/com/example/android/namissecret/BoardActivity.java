14a86994bdb132c4001732eab57743ce5bceb7e8
G-T-O <38909596+G-T-O@users.noreply.github.com>
2018-05-23 12:17:21 +0200

    Update BoardActivity.java

--- app/src/main/java/com/example/android/namissecret/BoardActivity.java
+++ app/src/main/java/com/example/android/namissecret/BoardActivity.java
@@ -2,37 +2,27 @@ package com.example.android.namissecret;
 import com.example.android.namissecret.R;
 import com.example.android.namissecret.fragments.MessengerItemFragment;
 import com.example.android.namissecret.fragments.MessengerListFragment;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TabHost;
 
-
 public class BoardActivity extends FragmentActivity {
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-
         TabHost tabHost;
-
         MessengerListFragment listFragment;
         MessengerItemFragment itemFragment;
-            listFragment = new MessengerListFragment();
-
-            itemFragment = new MessengerItemFragment();
+        listFragment = new MessengerListFragment();
+        itemFragment = new MessengerItemFragment();
 
 //        getFragmentManager().beginTransaction()
 //                .add(R.id.frameLayout, listFragment)
 //                .commit();
-
-
-            TabHost host = (TabHost)findViewById(R.id.tabHost);
-            host.setup();
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
 
             //Tab 1
             TabHost.TabSpec spec = host.newTabSpec("DRAGUER");
@@ -46,7 +36,6 @@ public class BoardActivity extends FragmentActivity {
             spec.setIndicator("",getResources().getDrawable(R.drawable.messenger_icon));
             host.addTab(spec);
 
-
             //Tab 3
             spec = host.newTabSpec("COMPTE");
             spec.setContent(R.id.tab3);
@@ -54,11 +43,8 @@ public class BoardActivity extends FragmentActivity {
             host.addTab(spec);
 
         }
-
-
         public void goBack(View view)
         {
             finish();
         }
     }
-
